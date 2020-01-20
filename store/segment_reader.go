2a19d7afc2c7401210fa7df91ab4913537f8a309
Dragan Milic <dragan@netice9.com>
2020-01-20 17:29:24 +0000

    store: WIP segment_reader

---
+++ store/segment_reader.go
@@ -0,0 +1,74 @@
+package store
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+// layout
+// total length: 4 bytes
+// type: byte
+// layer_sizes: 4 * 8 bytes
+// number_of_children: 1 byte
+// number_of_children * 8 bytes
+
+type SegmentReader []byte
+
+func NewSegmentReader(data []byte) (SegmentReader, error) {
+	if len(data) < 4 {
+		return SegmentReader{}, errors.New("segment data is too short")
+	}
+
+	totalLength := int(binary.BigEndian.Uint32(data))
+
+	if len(data) < totalLength {
+		return SegmentReader{}, errors.New("segment data is too short")
+	}
+
+	if totalLength < 4+1+4*8+1 {
+		return SegmentReader{}, errors.New("total length is too short")
+	}
+
+	numberOfChildren := data[4+1+4*8]
+
+	headerLength := numberOfChildren*8 + 4 + 1 + 4*8 + 1
+
+	if int(headerLength) > totalLength {
+		return SegmentReader{}, errors.New("total length is too short")
+	}
+
+	return data[:totalLength], nil
+
+}
+
+func (s SegmentReader) NumberOfChildren() int {
+	return int(s[4+1+4*8])
+}
+
+func (s SegmentReader) GetChildAddress(i int) Address {
+	if i < 0 {
+		panic("negative child index")
+	}
+
+	if i >= s.NumberOfChildren() {
+		panic("trying to get address of not existing child")
+	}
+
+	return Address(binary.BigEndian.Uint64(s[4+1+4*8+1+8*i:]))
+}
+
+func (s SegmentReader) GetData() []byte {
+	nc := s.NumberOfChildren()
+	return s[4+1+4*8+1+8*nc:]
+}
+
+func (s SegmentReader) GetLayerTotalSize(l int) uint64 {
+	if l < 0 {
+		panic("layer number can't be negative")
+	}
+	if l > 3 {
+		panic("layer number can't be >3")
+	}
+
+	return binary.BigEndian.Uint64(s[4+1+l*8:])
+}
